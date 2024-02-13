0b7325283270333c0315d5d5c3b77e5590e2a6dc
AhmedAlariqi97 <ahmedalariqi97@gmail.com>
2024-02-13 19:21:20 +0300

    add hero components

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -3,75 +3,101 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="flex-1 md:flex md:items-center md:gap-12">
-            <a className="block text-teal-600" href="#">
-              <span className="sr-only">Home</span>
+    
+    <header class="bg-white dark:bg-gray-900 shadow-md">
+      <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex-1 md:flex md:items-center md:gap-12">
+            <a class="block text-teal-600 dark:text-teal-300" href="#">
+              <span class="sr-only">Home</span>
               <Image src='/logo.svg' alt='logo' width={50} height={50} />
             </a>
           </div>
-
-          <div className="md:flex md:items-center md:gap-12">
-            <nav aria-label="Global" className="hidden md:block">
-              <ul className="flex items-center gap-6 text-sm">
+    
+          <div class="md:flex md:items-center md:gap-12">
+            <nav aria-label="Global" class="hidden md:block">
+              <ul class="flex items-center gap-6 text-sm">
                 <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About </a>
+                  <a
+                    class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Home
+                  </a>
                 </li>
-
+    
                 <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Careers </a>
+                  <a
+                    class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Explore
+                  </a>
                 </li>
-
+    
                 <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> History </a>
+                  <a
+                    class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Projects
+                  </a>
                 </li>
-
+    
                 <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Services </a>
+                  <a
+                    class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    About us
+                  </a>
                 </li>
-
+    
                 <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Projects </a>
+                  <a
+                    class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Contact us
+                  </a>
                 </li>
-
-                <li>
-                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Blog </a>
-                </li>
+    
+                
               </ul>
             </nav>
-
-            <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
+    
+            <div class="flex items-center gap-4">
+              <div class="sm:flex sm:gap-4">
                 <a
-                  className="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white shadow"
+                  class="rounded-md bg-primary hover:bg-primary/35 px-5 py-2.5 text-sm font-medium text-white shadow dark:hover:bg-teal-500"
                   href="#"
                 >
                   Login
                 </a>
-
-                <div className="hidden sm:flex">
+    
+                <div class="hidden sm:flex">
                   <a
-                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                    class="rounded-md bg-gray-100 hover:bg-gray-100/35 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
                     href="#"
                   >
                     Register
                   </a>
                 </div>
               </div>
-
-              <div className="block md:hidden">
-                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+    
+              <div class="block md:hidden">
+                <button
+                  class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
+                    class="h-5 w-5"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    strokeWidth="2"
+                    stroke-width="2"
                   >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
                 </button>
               </div>
