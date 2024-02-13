ba797a1399e2e688c116dea168d29da13a0a0e34
AhmedAlariqi97 <ahmedalariqi97@gmail.com>
2024-02-13 22:17:51 +0300

    edite rest api

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -6,18 +6,17 @@ function Hero() {
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Understand User Flow.
-            <strong className="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
+            All Your Digigal Products
+            <strong className="font-extrabold text-primary sm:block">Is One Click Away</strong>
           </h1>
 
           <p className="mt-4 sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-            numquam ea!
+            Start Exploring State of the Art Assets Now!
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-primary/50 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
               href="#"
             >
               Get Started
